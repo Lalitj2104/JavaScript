0bf28c624f8b3679699bc3c46cdf6b7cf14a779d
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-28 09:00:50 +0530

    commit

---
+++ file1.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("Hello world");
+
+var x=10;
+console.log(x)
+
+let y="200";
+console.log(y);
+
+let q="Coder ";
+console.log(q.length)
+
+ console.log(typeof (x+""))
+
+ console.log(typeof (+y))
+
+
+ var my=`My name is ${q} and age is ${x}`;
+ console.log(my)
+
+ console.log(Number.MAX_SAFE_INTEGER)
---
+++ loops.js
@@ -0,0 +1,29 @@
+// While loop
+// var i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+//sum of first n natural no.
+var sum=0;
+var i=1  ;
+while(i<=10){
+    sum+=i;
+    i++;
+}
+console.log(sum);
+
+
+
+// for loop
+for(let i=0;i<10;i++){
+    console.log(i);   
+}
+
+
+//doWhile loop
+
+var x=10;
+do{
+    console.log(`value of x is ${x}`);
+}while(x<=9);
