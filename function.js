46f44a72fd2673eedf718c7a4a6fac5df83ffc87
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-29 01:22:36 +0530

    commit

---
+++ function.js
@@ -0,0 +1,21 @@
+"use strict"
+
+//simple functions
+function SingSong(){
+    console.log("happy birthday to u");
+}
+
+SingSong();
+
+function plus(){
+    console.log(2+4);
+}
+plus();
+SingSong();
+
+//reusable func
+function sum(x,y){
+    return x+y;
+}
+
+console.log(sum(4,7))
--- object.js
+++ object.js
@@ -1,3 +1,4 @@
+"use strict"
 // creating of objects
 
 const person={
