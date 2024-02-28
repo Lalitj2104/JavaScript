84a30ee24e8469cafe1283abe88763662db835fe
Lalit2104 <lalitjindal0295@gmail.com>
2024-02-28 18:13:37 +0530

    commit

---
+++ object.js
@@ -0,0 +1,21 @@
+// creating of objects
+
+const person={
+    name:"coder",
+    age:20,
+    hobby:["coding","problem solving"]
+}
+
+
+//adding new pair
+ person.level="pro";
+ console.log(person);
+
+
+ person["gender"]="male";
+ 
+
+ //printing
+ console.log(person["age"]);
+
+ console.log(person);
