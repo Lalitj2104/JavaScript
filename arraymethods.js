48ca68a6d4d3c3ca0330c04f1b392e7cd5d6b782
Lalit2104 <lalitjindal0295@gmail.com>
2024-03-01 04:31:53 +0530

    commit

--- arraymethods.js
+++ arraymethods.js
@@ -35,3 +35,71 @@ const sq=function(num){
 
 const square=num.map(sq);
 console.log(square);
+
+
+//3.filter method
+
+const even=(number)=>{
+    if(number%2===0){
+        return number;
+    }
+}
+
+const evenNum=num.filter(even);
+console.log(evenNum);
+
+
+//4. reduce 
+// const aws=[1,2,3,4,5]
+
+// const sum=aws.reduce((accumulator,currentValue)=>{
+//     return accumulator+currentValue;
+// });
+// console.log(sum); 
+
+
+
+const userCart=[
+    {product:1,Name:"mobile",price:20000},
+    {product:2,Name:"laptop",price:22000},
+    {product:3,Name:"tv",price:15000},
+]
+
+const totalAmount=userCart.reduce((totalprice,currentprice)=>{
+    return totalprice+currentprice.price;
+},0)
+
+console.log(totalAmount);
+
+
+//sort function
+ const  numbers=[5,9,1200,410,300]
+ numbers.sort();
+ console.log(numbers);
+
+numbers.sort((a,b)=>a-b);
+console.log(numbers);
+
+
+//for sorting the userCart 
+userCart.sort((a,b)=>{
+    return a.price-b.price;
+});
+
+console.log(userCart);
+
+
+
+// find function 
+const myArray=["hello","cat","doggy","lion"]
+
+// function islength(string){
+//     return string.length ===3;
+// }
+
+// const ans=myArray.find(islength);
+
+const ans=myArray.find((string)=>{
+    return string.length ===3;
+})
+console.log(ans);
