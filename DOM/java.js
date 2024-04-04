e0a73fd31239d0b30315bfd3fbcb0f859fc79637
Lalit2104 <lalitjindal0295@gmail.com>
2024-04-04 14:36:58 +0530

    commit

---
+++ DOM/java.js
@@ -0,0 +1,27 @@
+const todoForm=document.querySelector(".form-todo")
+const todoinput=document.querySelector(".form-todo input[type='text']");
+const todolist=document.querySelector(".todo-list");
+todoForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const newtodotext=todoinput.value;
+    const newLi=document.createElement("li");
+    const newLiInnerHtml=`<span class="text">${newtodotext}</span>
+    <div class="todo-buttons">
+      <button class="todo-btn done">Done</button>
+      <button class="todo-btn remove">Remove</button>
+    </div>`;
+    newLi.innerHTML=newLiInnerHtml;
+    todolist.append(newLi);
+    todoinput.value="";
+})
+
+todolist.addEventListener("click",(e)=>{
+  if(e.target.classList.contains("done")){
+    const lispan=e.target.parentNode.previousElementSibling;
+    lispan.style.textDecoration="line-through";
+  }
+  if(e.target.classList.contains("remove")){
+    const targatedli=e.target.parentNode.parentNode;
+    todolist.remove(targatedli);
+  }
+})
